0fef590df8de09f4ec78e2c0090fea6de33c2188
Harshith Goka <harshith9399@gmail.com>
2019-02-07 19:30:09 +0530

    [App] handle app/uni links; site-association pending on iOS;

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
+#import <uni_links/UniLinksPlugin.h>
 
 @implementation AppDelegate
 
@@ -11,5 +12,13 @@
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+// NOTE: Necessary, until Flutter supports
+//       `application:continueUserActivity:restorationHandler` within the
+//       `FlutterPlugin` protocol.
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler {
+    return [[UniLinksPlugin sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+}
+
 
 @end
