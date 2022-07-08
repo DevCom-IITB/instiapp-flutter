7868494172927aea68f32145dbe8e56a70e7a33f
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-07-08 20:01:44 +0530

    chnages

--- lib/src/routes/communitypostpage.dart
+++ lib/src/routes/communitypostpage.dart
@@ -11,11 +11,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
 class CommunityPostPage extends StatefulWidget {
-  // final CommunityPost? communityPost;
   final String? test;
-  // final Future<CommunityPost?> communityFuture;
 
-  CommunityPostPage({this.test});
+  CommunityPostPage({required this.test});
 
   static void navigateWith(BuildContext context) {
     Navigator.push(
@@ -25,10 +23,8 @@ class CommunityPostPage extends StatefulWidget {
           name: "/post/",
         ),
         builder: (context) => CommunityPostPage(
-            // communityPost: communitypost,
-            test: "hello"
-            // //communityFuture: bloc.getCommunityDetails(community.id ?? ""),
-            ),
+          test: "community",
+        ),
       ),
     );
   }
@@ -54,7 +50,20 @@ class _CommunityPostPageState extends State<CommunityPostPage> {
     double _avatarRadius = 50;
 
     return Scaffold(
-      body: CommunityPostWidget(),
+      key: _scaffoldKey,
+      extendBodyBehindAppBar: true,
+      appBar: CustomAppBar(
+        transparentBackground: true,
+        searchIcon: true,
+        appBarSearchStyle: AppBarSearchStyle(hintText: "Search "),
+        leadingStyle: LeadingStyle(
+          icon: Icons.arrow_back,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      drawer: NavDrawer(),
       bottomNavigationBar: MyBottomAppBar(
         shape: RoundedNotchedRectangle(),
         child: new Row(
@@ -74,6 +83,112 @@ class _CommunityPostPageState extends State<CommunityPostPage> {
           ],
         ),
       ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CommunityPostSection(),
+              ],
+            ),
+            Positioned(
+              top: 200 - _avatarRadius,
+              left: 20,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed("/posts/add");
+        },
+        child: Icon(Icons.create_outlined),
+      ),
     );
+  }
+
+  Widget _buildInfo(ThemeData theme) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      ),
+    );
+  }
+}
+
+class CommunityAboutSection extends StatefulWidget {
+  final CommunityPost? community;
+
+  const CommunityAboutSection({Key? key, required this.community})
+      : super(key: key);
+
+  @override
+  State<CommunityAboutSection> createState() => CommunityAboutSectionState();
+}
+
+class CommunityAboutSectionState extends State<CommunityAboutSection> {
+  int _selectedIndex = 0;
+
+  bool aboutExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+      ),
+    );
+  }
+}
+
+class CommunityPostSection extends StatefulWidget {
+  const CommunityPostSection({Key? key}) : super(key: key);
+
+  @override
+  State<CommunityPostSection> createState() => _CommunityPostSectionState();
+}
+
+class _CommunityPostSectionState extends State<CommunityPostSection> {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: theme.colorScheme.surfaceVariant.withOpacity(0.4)),
+          ),
+          Divider(
+            color: theme.colorScheme.onSurfaceVariant,
+            height: 0,
+          ),
+          Container(
+            decoration: BoxDecoration(color: theme.colorScheme.surfaceVariant),
+            child: Column(
+              children: _buildPostList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildPostList() {
+    return [CommunityPostWidget()];
+  }
+
+  List<Widget> _buildCommentList() {
+    return [CommunityCommentWidget(), CommunityCommentWidget()];
   }
 }
