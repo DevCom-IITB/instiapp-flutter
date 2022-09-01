161f2b38c6bff75b9a7f89aa88ed51e7085e27bf
Palash Mittal <pmittal00@gmail.com>
2022-09-01 22:38:47 +0530

    Setup interceptors for 400 and 401 error responses

---
+++ lib/src/api/interceptors.dart
@@ -0,0 +1,29 @@
+import 'package:InstiApp/main.dart';
+import 'package:InstiApp/src/bloc_provider.dart';
+import 'package:InstiApp/src/blocs/ia_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class ErrorInterceptor extends Interceptor {
+  BuildContext context;
+  GlobalKey<NavigatorState> navigatorKey;
+
+  ErrorInterceptor({required this.context, required this.navigatorKey});
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    // print("Error");
+    final res = err.response;
+    InstiAppBloc bloc = BlocProvider.of(context)!.bloc;
+    if (res != null) {
+      // print("Res not null: " + res.statusCode.toString());
+      if (res.statusCode == 400 || res.statusCode == 401) {
+        // print("Logging out");
+        await bloc.logout();
+        navigatorKey.currentState?.pushReplacementNamed('/',
+            arguments: "You have been logged out! Please login again.");
+      }
+    }
+    handler.next(err);
+  }
+}
