fb3e7fda1c026cad68b6eadb062d48331c07f955
AshwinRamachandran2002 <200050016@iitb.ac.in>
2022-11-16 19:07:33 +0530

    api change

--- lib/src/api/apiclient.g.dart
+++ lib/src/api/apiclient.g.dart
@@ -8,7 +8,7 @@ part of 'apiclient.dart';
 
 class _InstiAppApi implements InstiAppApi {
   _InstiAppApi(this._dio, {this.baseUrl}) {
-    baseUrl ??= 'https://api.insti.app/api';
+    baseUrl ??= 'https://gymkhana.iitb.ac.in/instiapp/api';
   }
 
   final Dio _dio;
