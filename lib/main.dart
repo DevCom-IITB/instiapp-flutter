6e89ac911332456757cf68b674874d81be4d0810
Palash Mittal <pmittal00@gmail.com>
2022-12-19 14:29:03 +0530

    feat(upgrader): add upgrader alert in drawer

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ import 'package:uni_links/uni_links.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:upgrader/upgrader.dart';
 
 void main() async {
   // print("Runnning main");
