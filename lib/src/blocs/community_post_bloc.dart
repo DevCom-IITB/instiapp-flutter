01c09337f5938b5308997c13385c7f27f9db646c
Palash Mittal <pmittal00@gmail.com>
2023-07-24 23:29:41 +0530

    fix(community): null check in post bloc refresh

--- lib/src/blocs/community_post_bloc.dart
+++ lib/src/blocs/community_post_bloc.dart
@@ -41,11 +41,15 @@ class CommunityPostBloc {
         bloc.getSessionIdHeader(), post.id!, reaction);
   }
 
-  Future refresh( { CPType type = CPType.All, required  String id}) async {
-
+  Future refresh({CPType type = CPType.All, required String? id}) async {
     // _communityPosts = defCommunities;
     // _communitySubject.add(defCommunities);
     // print("refresh");
+    if (id == null) {
+      _communitySubject.add([]);
+      return;
+    }
+
     int? status;
     switch (type) {
       case CPType.All:
@@ -61,8 +65,8 @@ class CommunityPostBloc {
         status = null;
         break;
     }
-    _communityPosts = (await bloc.client
-                .getCommunityPosts(bloc.getSessionIdHeader(), status, query, id))
+    _communityPosts = (await bloc.client.getCommunityPosts(
+                bloc.getSessionIdHeader(), status, query, id))
             .data ??
         [];
 
