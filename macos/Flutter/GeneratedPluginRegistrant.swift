503a33030bed45d0d30cbe4f2578d49f8380a093
Jatin Singhal <jatinsinghal@Jatins-MacBook-Air-2.local>
2024-01-01 20:11:27 +0530

    Pulled Master

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,19 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus_macos
+import file_selector_macos
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import flutter_native_timezone
 import location
 import package_info_plus_macos
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
