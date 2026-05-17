5e54df9afc553b3f1eb154c94bb704e9d2f88a93
Jyotirya <jyotiraagrawal@gmail.com>
2026-05-17 19:34:04 +0530

    Notifications fixed

--- lib/src/api/apiclient.dart
+++ lib/src/api/apiclient.dart
@@ -60,7 +60,7 @@ import 'model/offersecret.dart';
 part 'apiclient.g.dart';
 
 // @rt.RestApi(baseUrl: "http://127.0.0.1:8000/api")
-// @rt.RestApi(baseUrl: "https://5427-103-21-126-76.ngrok-free.app/api")
+// @rt.RestApi(baseUrl: "https://035b-2401-4900-aa02-ddb0-5df9-3d94-6b84-937f.ngrok-free.app/api")
 @rt.RestApi(baseUrl: "https://gymkhana.iitb.ac.in/instiapp/api")
 //@rt.RestApi(baseUrl: "https://nayeli-nonbulbar-denominationally.ngrok-free.dev/api")
 // @rt.RestApi(baseUrl: "https://f6ba722a576a.ngrok-free.app/api")
